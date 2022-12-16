0739e404290c2373c49e28547d1aa2c76e356931
flouresscent <68514544+flouresscent@users.noreply.github.com>
2022-12-16 13:37:25 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ void xtea_encipher(unsigned int num_rounds, uint32_t v[2], uint32_t const key[4]
     for (i = 0; i < num_rounds; i++) {
         v0 += (((v1 << 4) ^ (v1 >> 5)) + v1) ^ (sum + key[sum & 3]);
         sum += delta;
-        v1 += (((v0 << 4) ^ (v0 >> 5)) + v0) ^ (sum + key[(sum>>11) & 3]);
+        v1 += (((v0 << 4) ^ (v0 >> 5)) + v0) ^ (sum + key[(sum >> 11) & 3]);
     }
     
     v[0] = v0; v[1] = v1;
@@ -25,7 +25,7 @@ void xtea_decipher(unsigned int num_rounds, uint32_t v[2], uint32_t const key[4]
     uint32_t v0 = v[0], v1=v[1], delta = 0x9E3779B9, sum = delta * num_rounds;
     
     for (i = 0; i < num_rounds; i++) {
-        v1 -= (((v0 << 4) ^ (v0 >> 5)) + v0) ^ (sum + key[(sum>>11) & 3]);
+        v1 -= (((v0 << 4) ^ (v0 >> 5)) + v0) ^ (sum + key[(sum >> 11) & 3]);
         sum -= delta;
         v0 -= (((v1 << 4) ^ (v1 >> 5)) + v1) ^ (sum + key[sum & 3]);
     }
@@ -37,9 +37,9 @@ void xtea_decipher(unsigned int num_rounds, uint32_t v[2], uint32_t const key[4]
 void encrypt_decrypt(char *inout, int len, bool encrypt) {
     for(int i = 0; i < len / BLOCK_SIZE; i++) {
         if (encrypt)
-            xtea_encipher(32,(uint32_t*)(inout+(i*BLOCK_SIZE)),key);
+            xtea_encipher(32, (uint32_t*)(inout + (i * BLOCK_SIZE)), key);
         else
-            xtea_decipher(32,(uint32_t*)(inout+(i*BLOCK_SIZE)),key);
+            xtea_decipher(32, (uint32_t*)(inout + (i * BLOCK_SIZE)), key);
     }
   
     if (len % BLOCK_SIZE != 0) {
